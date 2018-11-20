d72319e20146d2e5db10ee2816bd2f7f86405f74
shroyer.shawn@gmail.com <41597227+ncredsquirrel@users.noreply.github.com>
2018-11-20 10:26:30 -0500

    Added Morris Charts
    Added Morris charts with one "working", still needs to be tested.
    Created place holder for a second Morris chart that need to be implemented.

---
+++ BudgetDestroyer/Controllers/MorrisController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BudgetDestroyer.Models;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
+
+namespace BudgetDestroyer.Controllers
+{
+    [Authorize]
+    public class MorrisController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult GetBudgetDataForBarChart()
+        {
+            var budgetData = new List<MorrisBudgetBar>();
+            var userId = User.Identity.GetUserId();
+            var houseId = db.Users.Find(userId).HouseholdId;
+            var budgets = db.Households.Find(houseId).Budgets.ToList();
+            var budgetItems = db.BudgetItems.Where(i => db.Budgets.Any(b => b.Id == i.BudgetId && b.HouseholdId == houseId)).ToList();
+
+            if (houseId == null)
+            {
+                return Content(JsonConvert.SerializeObject(budgetData), "application/json");
+            }
+
+            
+
+            foreach (var budget in budgets) {
+                foreach(var item in budgetItems)
+                {
+
+                }
+            }
+
+            //var transactions = db.Transactions.Include(t => t.EnteredBy).Include(t => t.HouseAccount).Include(t => t.TransactionType);
+
+            foreach (var budget in budgets)
+            {
+                var temp = db.Transactions.Where(t => db.BudgetItems.Any(i => i.Id == t.BudgetItemId && budget.Id == i.BudgetId));
+                decimal amount = 0;
+                foreach (var tempTran in temp)
+                {
+                    if (tempTran.Amount < 0)
+                    {
+                        amount += tempTran.Amount;
+                    }
+                }
+                amount *= -1;
+
+                budgetData.Add(new MorrisBudgetBar
+                {
+                    Label = budget.Name,
+                    Target = (budget.Amount + amount),
+                    Actual = budget.Amount
+                });
+            }
+
+            return Content(JsonConvert.SerializeObject(budgetData), "application/json");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BudgetDestroyer/Models/MorrisBudgetBar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetDestroyer.Models
+{
+    public class MorrisBudgetBar
+    {
+        public string Label { get; set; }
+        public decimal Target { get; set; }
+        public decimal Actual { get; set; }
+    }
+}
